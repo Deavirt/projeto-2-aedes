78459c7a8ba7825838517e1ecd9aa44c3ed122fe
Deavirt <davirtpet@gmail.com>
2024-03-13 14:22:09 -0300

    Organização de arquivos

---
+++ Soma-2-números.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int x, y, result, cont;
+	cont = 1;
+
+	while (cont <=3 ) {
+		printf("Entre com dois números para serem somados: ");
+		fflush(stdout);
+		scanf("%d %d" , &y, &x);
+
+		result= x + y;
+
+		printf("O resultado da soma é %d\n", result);
+
+		cont = cont + 1;
+	}
+
+	for (cont = 1; cont <=3; cont++) {
+		printf("Entre com dois números para serem somados: ");
+		fflush(stdout);
+		scanf("%d %d" , &x ,&y);
+
+		result = x + y;
+
+		printf("O resultado da soma é:%d\n", result);
+
+		cont = 1;
+		do {
+			printf("Entre com dois números para serem somados");
+			fflush(stdout);
+			scanf("%d %d" , &x ,&y);
+
+			result = x + y;
+
+			printf(" O valeor da soma é %d\n", result);
+		}while (cont <= 3);
+
+	}
+		return 0;
+}
